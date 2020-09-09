b5d831ca34ad4797648acc9a6dd8db46a3a39829
Higor Cavalcanti <higordaniell@live.com>
2020-09-09 20:16:32 -0300

    Renomeando projeto

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('test-btg app is running!');
+    expect(page.getTitleText()).toEqual('simple-client-manager app is running!');
   });
 
   afterEach(async () => {
